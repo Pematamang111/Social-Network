bb64a864797bb4ae3cedbeeeb42b6cb3d7cc98e3
Pema Sherpa <pematamang111@gmail.com>
2024-04-25 18:55:07 -0400

    add model of thought and user

---
+++ models/thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('Schema');
+
+const thoughtSchema = new Schema(
+    {
+           thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength:  280,
+           },
+           createdAt: {
+            type: Date,
+            default: Date.now,//use getter method to format timestamp
+           },
+           username: {//the user that created this thought 
+            type: String,
+            required: true,
+           },
+           reactions: [reactionSchema],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+) 
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function() {
+    return this.thoughtText; //retrieves the length of the thought's reactions array field on query.
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
